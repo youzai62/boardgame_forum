5acd325a6fb2bdbb03dcefd9852672c9dea83eae
Roy Zhu <zhujiajun62@gmail.com>
2022-04-13 22:26:40 -0700

    Login, Logout, Profile implement but no css

--- starter/Frontend/src/components/Header.js
+++ starter/Frontend/src/components/Header.js
@@ -1,18 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component, Profiler } from 'react';
 import logo from '../logo.svg';
 import '../css/Header.css';
+import LoginButton from './LoginButton';
+import LogoutButton from './LogoutButton';
+import Profile from './Profile';
 
 class Header extends Component {
 
     navTo(uri){
       window.location.href = window.location.origin + uri;
     }
-  
+
+    
     render() {
+      
       return (
         <div className="App-header">
           <h1 onClick={() => {this.navTo('')}}>Boardgame Forum</h1>
-          <h2 onClick={() => {this.navTo('/login')}}>login</h2>
+          <Profile />
+          <LoginButton />
+          <LogoutButton />
         </div>
       );
     }
--- starter/Frontend/src/index.js
+++ starter/Frontend/src/index.js
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Auth0Provider } from "@auth0/auth0-react";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<Auth0Provider
+    domain="dev-royzhu.us.auth0.com"
+    clientId="wRJEcBy466MQx6VeQzzem1kSYs3CxpCG"
+    redirectUri={window.location.origin}
+  >
+    <App />
+</Auth0Provider>, 
+document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
